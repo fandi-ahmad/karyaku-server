db64d2446c0fbcf03bb171c22e88df8ae96a9f0f
fandi-ahmad <fandi4160@gmail.com>
2023-09-15 10:12:04 +0800

    add update and delete project api

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,4 +1,4 @@
-const { User_project } = require('../models')
+const { User_project, sequelize } = require('../models')
 const { v4: uuidv4 } = require('uuid')
 const path = require('path')
 const fs = require('fs')
@@ -14,8 +14,6 @@ const removeImage = (filePath) => {
 const createProject = async (req, res) => {
   try {
     const { uuid_user, title, description, tag, demo_link, source_code } = req.body
-    console.log(req, '<-- semua request');
-    console.log(req.file, '<-- request file jika ada');
     const image_upload = req.file.path
     const randomUUID = uuidv4()
 
@@ -57,4 +55,81 @@ const getProjectListByUser = async (req, res) => {
   }
 }
 
-module.exports = { createProject, getProjectListByUser }
+const getAllProjectList = async (req, res) => {
+  try {
+    const rawQuery = /*sql*/`
+      SELECT 
+        user_projects.id, user_projects.uuid, users.username, user_profiles.profile_picture,
+        user_projects.project_image, user_projects.title, user_projects.description, 
+        user_projects.demo_link, user_projects.source_code
+      FROM users
+      JOIN user_profiles ON (users.uuid = user_profiles.uuid_user)
+      JOIN user_projects ON (users.uuid = user_projects.uuid_user)
+    `
+
+    const dataProject = await sequelize.query(rawQuery)
+    res.status(200).json({ status: 200, message: 'ok', data: dataProject[0] })
+  } catch (error) {
+    res.status(500).json({ status: 500, message: 'Internal server error' });
+    console.log(error, '<-- error get project list');
+  }
+}
+
+const updateProject = async (req, res) => {
+  try {
+    const { uuid_user, uuid, title, description, tag, demo_link, source_code } = req.body
+
+    const userProject = await User_project.findAll({
+      where: { uuid: uuid }
+    })
+
+    if (req.file) {
+      const image_upload = req.file.path
+      if (userProject[0].project_image === '') {
+        userProject[0].project_image = image_upload
+      } else {
+        removeImage(userProject[0].project_image)
+        userProject[0].project_image = image_upload
+      }
+    }
+
+    userProject[0].title = title
+    userProject[0].description = description
+    userProject[0].tag = tag
+    userProject[0].demo_link = demo_link
+    userProject[0].source_code = source_code
+    userProject[0].save()
+
+    res.status(200).json({ status: 200, message: 'update project successfully', data: userProject[0] })
+
+  } catch (error) {
+    res.status(500).json({ status: 500, message: 'Internal server error' });
+    console.log(error, '<-- error update project');
+  }
+}
+
+const deleteProject = async (req, res) => {
+  try {
+    const { uuid } = req.params
+    const userProject = await User_project.findAll({
+      where: { uuid: uuid }
+    })
+
+    if (!userProject[0]) {
+      return res.status(404).json({
+        status: 404,
+        message: 'data is not found'
+      })
+    }
+
+    userProject[0].destroy()
+    res.json({ status: 200, message: 'delete successfully' })
+    
+  } catch (error) {
+    res.status(500).json({ status: 500, message: 'Internal server error' });
+    console.log(error, '<-- error delete project');
+  }
+}
+
+
+module.exports = { createProject, getProjectListByUser, getAllProjectList, updateProject, deleteProject }
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -1,9 +1,12 @@
 const router = require("express").Router()
-const { createProject, getProjectListByUser } = require('../controllers/projectController')
+const { createProject, getProjectListByUser, getAllProjectList, updateProject, deleteProject } = require('../controllers/projectController')
 const { addAuthorization } = require('../middleware/verifyToken')
 
+router.get('/', getAllProjectList)
 router.post('/create', addAuthorization, createProject)
 router.get('/:uuid_user', addAuthorization, getProjectListByUser)
+router.put('/update', updateProject)
+router.delete('/delete/:uuid', deleteProject)
 
 
 module.exports = router
